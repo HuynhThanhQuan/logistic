a2b38cdd4b7520b3a23344ef0b261a101c2a1366
quan <quan@kali.quan.com>
2020-02-03 12:03:33 -0500

    Update

---
+++ format/data_12_4_tsptw.sol
@@ -0,0 +1,21 @@
+NAME : data_12_4_tsptw.tsptw.8397.tour
+COMMENT : Length = 8397
+COMMENT : Found by LKH [Keld Helsgaun] Mon Feb  3 11:32:05 2020
+TYPE : TOUR
+DIMENSION : 13
+TOUR_SECTION
+1
+3
+5
+8
+12
+2
+11
+6
+10
+9
+7
+4
+13
+-1
+EOF
---
+++ format/data_9_2_tsptw.sol
@@ -0,0 +1,18 @@
+NAME : data_9_2_tsptw.tsptw.7241.tour
+COMMENT : Length = 7241
+COMMENT : Found by LKH [Keld Helsgaun] Mon Feb  3 12:02:39 2020
+TYPE : TOUR
+DIMENSION : 10
+TOUR_SECTION
+1
+10
+9
+4
+2
+3
+7
+8
+5
+6
+-1
+EOF
